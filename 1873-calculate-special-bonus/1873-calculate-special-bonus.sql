7a872125a5b2f5256981a2c520e282b5e9f2f0ef
mohmmedfathi <64088888+mohmmedfathi@users.noreply.github.com>
2022-07-29 22:31:59 +0200

    Update 1873-calculate-special-bonus.sql

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -6,3 +6,9 @@ CASE
  END AS bonus 
  FROM Employees
  ORDER BY employee_id
+ 
+ -- another sol by if 
+ SELECT employee_id,
+if(employee_id % 2 AND name NOT LIKE 'M%', salary, 0) AS bonus
+from Employees 
+ORDER BY employee_id;
